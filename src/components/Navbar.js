c309b5b102526d2b08c108d91864ff675e53e629
Matthew Lee <mattjhlee25@gmail.com>
2023-12-03 22:17:05 -0500

    set up navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,33 @@
 import React from "react";
 
 function Navbar() {
-    
+    return (
+        <nav>
+            <div>
+                
+                <Link to="/">
+                    <span className="home-link">
+                    <h3>Home</h3>
+                    </span>
+                </Link>
+                <Link to="/projects">
+                    <span className="projects-link">
+                    <h3>About Us</h3>
+                    </span>
+                </Link>
+                <Link to="/aboutme">
+                    <span className="aboutme-link">
+                    <h3>Menu</h3>
+                    </span>
+                </Link>
+                <Link to="/contact">
+                    <span className="contactr-link">
+                    <h3>Special Orders</h3>
+                    </span>
+                </Link>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar;
