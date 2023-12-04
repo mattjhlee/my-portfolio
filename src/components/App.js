e71f229547fa31f8a689e50c19270a557e093ef0
Matthew Lee <mattjhlee25@gmail.com>
2023-12-03 22:21:29 -0500

    add projects to Navbar and routes. set up basic return statement in proejcts

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import {Routes, Route} from "react-router-dom";
 import './App.css';
-import Navbar from './Navbar';
+import Projects from './Projects';
 import AboutMe from './AboutMe';
 import Contact from './Contact';
+
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/projects' element={<Projects />}></Route>
         <Route path='/aboutme' element={<AboutMe />}></Route>
         <Route path='/contact' element={<Contact />}></Route>
       </Routes>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,11 @@ import React from "react";
 import Navbar from "./Navbar";
 
 function Projects() {
-    
+    return (
+        <div className="projects">
+            <Navbar />
+        </div>
+    )
 }
 
 export default Projects;
